93b9db8ab900e0345e9e4b059ea8feb23acec440
clayy <clarenceolayta.work@gmail.com>
2026-04-05 18:24:24 +0800

    updated directories

--- media-scope-ph/src/App.jsx
+++ media-scope-ph/src/App.jsx
@@ -6,13 +6,8 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="bg-gray-100">
+    <div className="h-screen bg-gray-100">
       <Navbar />
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-
-      <svg className="h-6 w-6">
-        <use href="/icons.svg#github-icon" />
-      </svg>
     </div>
   );
 }
