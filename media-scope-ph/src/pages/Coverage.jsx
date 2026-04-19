ffff425131b10aac336b92466bb6cbd09e9bc3f4
clayy <clarenceolayta.work@gmail.com>
2026-04-19 18:56:20 +0800

    added layouts for coverage and report page

--- media-scope-ph/src/pages/Coverage.jsx
+++ media-scope-ph/src/pages/Coverage.jsx
@@ -1,13 +1,39 @@
-const error = true;
-
 const Coverage = () => {
   return (
     <main className="flex-1">
-      About Page
-      {error && <p className="text-red-500">An error occurred.</p>}
-      <p className={`${error ? "text-red-500" : "text-green-500"} text-2xl`}>
-        Error is Shown
-      </p>
+      <div className="flex flex-col items-center justify-center">
+        <div className="w-5/6 md:w-4/5">
+          <h1 className="text-2xl font-semibold text-slate-900 dark:text-slate-100">
+            Cross-Media Sentiment Dashboard
+          </h1>
+          <p>
+            Explore sentiment distribution and trends across Philippine news
+            outlets
+          </p>
+        </div>
+        <section className="mt-6 grid w-5/6 grid-cols-[1fr_3fr] gap-4 md:w-4/5">
+          <div className="space-y-2 rounded-xl bg-white p-8 text-justify shadow-md dark:bg-slate-900">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Temporibus praesentium eveniet sit fugit omnis quo amet, pariatur
+              quas accusantium debitis laboriosam sapiente aperiam dolores
+              delectus, minima, voluptates eum dolorem sequi. Est, quisquam
+              officiis. Odio repellendus sed totam. Nam veritatis fuga cumque
+              fugit, tenetur
+            </p>
+          </div>
+          <div className="space-y-2 rounded-xl bg-white p-8 text-justify shadow-md dark:bg-slate-900">
+            <p>
+              veniam omnis dicta. Nulla dolorum eveniet perferendis tempore,
+              quisquam maxime nihil esse, quas, libero molestiae magnam debitis.
+              Voluptatibus quos, fugiat assumenda harum ex nisi illum, deleniti
+              eligendi consequatur earum est asperiores eaque sequi aperiam
+              incidunt laborum voluptatem. Possimus, sit! Quis laudantium
+              laboriosam et maiores unde, corrupti vitae?
+            </p>
+          </div>
+        </section>
+      </div>
     </main>
   );
 };
--- media-scope-ph/src/pages/Report.jsx
+++ media-scope-ph/src/pages/Report.jsx
@@ -1,7 +1,34 @@
 const Report = () => {
   return (
     <main className="flex-1">
-      <p>Hello</p>
+      <div className="flex flex-col items-center justify-center">
+        <div className="w-5/6 md:w-4/5">
+          <h1 className="text-2xl font-semibold text-slate-900 dark:text-slate-100">
+            Report Details
+          </h1>
+          <p>
+            Sentence-level entity sentiment analysis results and real-time
+            testing
+          </p>
+        </div>
+        <section className="mt-6 w-5/6 space-y-2 rounded-xl bg-white p-8 text-justify shadow-md md:w-4/5 dark:bg-slate-900">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus
+            praesentium eveniet sit fugit omnis quo amet, pariatur quas
+            accusantium debitis laboriosam sapiente aperiam dolores delectus,
+            minima, voluptates eum dolorem sequi. Est, quisquam officiis. Odio
+            repellendus sed totam. Nam veritatis fuga cumque fugit, tenetur
+          </p>
+          <p>
+            veniam omnis dicta. Nulla dolorum eveniet perferendis tempore,
+            quisquam maxime nihil esse, quas, libero molestiae magnam debitis.
+            Voluptatibus quos, fugiat assumenda harum ex nisi illum, deleniti
+            eligendi consequatur earum est asperiores eaque sequi aperiam
+            incidunt laborum voluptatem. Possimus, sit! Quis laudantium
+            laboriosam et maiores unde, corrupti vitae?
+          </p>
+        </section>
+      </div>
     </main>
   );
 };
