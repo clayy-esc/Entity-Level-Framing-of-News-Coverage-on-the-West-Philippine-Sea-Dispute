a91cef9d2a02874344bc9f5e0e4e316806e9aa8b
Clayy <clarenceolayta.work@gmail.com>
2026-05-09 21:22:55 +0800

    updated link

--- media-scope-ph/backend/main.py
+++ media-scope-ph/backend/main.py
@@ -15,7 +15,7 @@ def on_startup():
 # CORS (support multiple origins)
 origins = [
     "http://localhost:5173",
-    "https://media-scope-ph.vercel.app/"
+    "https://entity-level-framing-of-news.vercel.app"
 ]
 
 # Remove None values (important)
